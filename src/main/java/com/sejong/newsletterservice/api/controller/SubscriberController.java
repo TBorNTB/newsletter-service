ed48e8a70656116a999073c8b1c15f645cdc92f4
PlusUltraCode <kkd06144@naver.com>
2025-06-15 19:15:10 +0900

    #1 feat: 구독관련 Controller 구현 완료

--- src/main/java/com/sejong/newsletterservice/api/controller/SubscriberController.java
+++ src/main/java/com/sejong/newsletterservice/api/controller/SubscriberController.java
@@ -1,7 +1,12 @@
 package com.sejong.newsletterservice.api.controller;
 
+import com.sejong.newsletterservice.api.controller.dto.request.SubscriberRequest;
+import com.sejong.newsletterservice.api.controller.dto.response.SubscriberResponse;
 import com.sejong.newsletterservice.application.service.SubscriberService;
+import com.sejong.newsletterservice.domain.model.vo.SubscriberRequestVO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +21,11 @@ public class SubscriberController {
     private final SubscriberService subscriberService;
 
     @PostMapping("/subscribe")
-    public ResponseEntity<Object> subscribe(
-            @RequestBody
-    )
+    public ResponseEntity<SubscriberResponse> subscribe(
+            @Valid @RequestBody SubscriberRequest subscriberRequest
+    ) {
+        SubscriberRequestVO subscriberRequestVO = subscriberRequest.toVO();
+        SubscriberResponse response = subscriberService.register(subscriberRequestVO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
---
+++ src/main/java/com/sejong/newsletterservice/domain/model/vo/SubscriberRequestVO.java
@@ -0,0 +1,4 @@
+package com.sejong.newsletterservice.domain.model.vo;
+
+public class SubscriberRequestVO {
+}
