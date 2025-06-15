e686973c21b38a856554f02bab28815c341cb4f4
PlusUltraCode <kkd06144@naver.com>
2025-06-15 19:17:14 +0900

    #1 feat: 구독관련 Service 구현 완료

--- src/main/java/com/sejong/newsletterservice/application/service/SubscriberService.java
+++ src/main/java/com/sejong/newsletterservice/application/service/SubscriberService.java
@@ -1,9 +1,29 @@
 package com.sejong.newsletterservice.application.service;
 
+import com.sejong.newsletterservice.api.controller.dto.response.SubscriberResponse;
+import com.sejong.newsletterservice.domain.model.MailCategory;
+import com.sejong.newsletterservice.domain.model.Subscriber;
+import com.sejong.newsletterservice.domain.model.vo.SubscriberRequestVO;
+import com.sejong.newsletterservice.domain.repository.MailCategoryRepository;
 import com.sejong.newsletterservice.domain.repository.SubscriberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class SubscriberService {
-    private final SubscriberRepository subscriberRepository;
+    private final MailCategoryRepository mailCategoryRepository;
+
+    public SubscriberResponse register(SubscriberRequestVO subscriberRequestVO) {
+        Subscriber subscriber = Subscriber.from(subscriberRequestVO, LocalDateTime.now());
+        List<MailCategory> mailCategories = subscriberRequestVO.selectedCategories().stream()
+                .map(MailCategory::of)
+                .toList();
+
+        Subscriber resultSubscriber = mailCategoryRepository.save(subscriber, mailCategories);
+        return SubscriberResponse.from(subscriber);
+    }
 }
