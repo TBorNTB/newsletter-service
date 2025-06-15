8c3bc7a0050802b3c23845099d6a4f41851ea509
PlusUltraCode <kkd06144@naver.com>
2025-06-15 19:18:47 +0900

    #1 feat: 구독관련 Repository 구현 완료

--- src/main/java/com/sejong/newsletterservice/domain/repository/MailCategoryRepository.java
+++ src/main/java/com/sejong/newsletterservice/domain/repository/MailCategoryRepository.java
@@ -1,4 +1,12 @@
 package com.sejong.newsletterservice.domain.repository;
 
+import com.sejong.newsletterservice.domain.model.MailCategory;
+import com.sejong.newsletterservice.domain.model.Subscriber;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface MailCategoryRepository {
+    Subscriber save(Subscriber subscriber, List<MailCategory> mailCategories);
 }
--- src/main/java/com/sejong/newsletterservice/infrastructure/persistence/category/JpaMailCategoryRepository.java
+++ src/main/java/com/sejong/newsletterservice/infrastructure/persistence/category/JpaMailCategoryRepository.java
@@ -1,11 +1,28 @@
 package com.sejong.newsletterservice.infrastructure.persistence.category;
 
+import com.sejong.newsletterservice.domain.model.MailCategory;
+import com.sejong.newsletterservice.domain.model.Subscriber;
+import com.sejong.newsletterservice.domain.repository.MailCategoryRepository;
+import com.sejong.newsletterservice.infrastructure.persistence.subscriber.SubscriberEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
-public class JpaMailCategoryRepository {
+public class JpaMailCategoryRepository implements MailCategoryRepository {
     private final SpringDataJpaMailCategoryRepository mailCategoryRepository;
 
+    @Override
+    public Subscriber save(Subscriber subscriber, List<MailCategory> mailCategories) {
+        SubscriberEntity subscriberEntity = SubscriberEntity.from(subscriber);
+
+        List<MailCategoryEntity> categoryEntities = mailCategories.stream()
+                .map(category -> MailCategoryEntity.from(category, subscriberEntity))
+                .toList();
+
+        mailCategoryRepository.saveAll(categoryEntities);
+        return subscriberEntity.toDomain();
+    }
 }
