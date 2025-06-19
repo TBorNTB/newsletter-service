926d4fc595644010a5a20159e07b0557203e158d
PlusUltraCode <kkd06144@naver.com>
2025-06-19 15:11:12 +0900

    #2 feat: 메일관련 EmailService 구현 - 실제 메일 전송하는 서비스스

--- src/main/java/com/sejong/newsletterservice/application/service/EmailService.java
+++ src/main/java/com/sejong/newsletterservice/application/service/EmailService.java
@@ -1,4 +1,21 @@
 package com.sejong.newsletterservice.application.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendVerificationEmail(String to, String code) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Verification Email");
+        message.setText("Your Verfication code is: "+code);
+        mailSender.send(message);
+    }
 }
