2e01036aeb9627a2e59c4fe715463d657ee8fcb6
PlusUltraCode <kkd06144@naver.com>
2025-06-20 09:59:05 +0900

    #2 chore: Swagger.ui 추가

--- src/main/java/com/sejong/newsletterservice/api/controller/SubscriberController.java
+++ src/main/java/com/sejong/newsletterservice/api/controller/SubscriberController.java
@@ -17,10 +17,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/newsletter")
 public class SubscriberController {
