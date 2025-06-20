ad024d4f5f1bd5e0fe7cceb8f3610ee4d11f5989
PlusUltraCode <kkd06144@naver.com>
2025-06-20 09:36:26 +0900

    #2 feat: 메일전송 관련 비동기 처리  구현

--- src/main/java/com/sejong/newsletterservice/application/service/EmailService.java
+++ src/main/java/com/sejong/newsletterservice/application/service/EmailService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -18,7 +19,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
-
+    @Async
     public void sendVerificationEmail(String to, String code) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
