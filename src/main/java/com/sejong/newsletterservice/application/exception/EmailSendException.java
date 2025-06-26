8d9c3483c8c02006e855417d5f3c0d948a03efde
PlusUltraCode <kkd06144@naver.com>
2025-06-26 16:44:14 +0900

    #9 feat: GlobalHandler Test 구현 완료

--- src/main/java/com/sejong/newsletterservice/application/exception/EmailSendException.java
+++ src/main/java/com/sejong/newsletterservice/application/exception/EmailSendException.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 public class EmailSendException extends RuntimeException {
+    public EmailSendException(String message) {
+        super(message);
+    }
+
     public EmailSendException(String message, Throwable cause) {
         super(message, cause);
     }
---
+++ src/test/java/com/sejong/newsletterservice/application/exception/GlobalErrorHandlerTest.java
@@ -0,0 +1,37 @@
+package com.sejong.newsletterservice.application.exception;
+
+import com.sejong.newsletterservice.application.controller.TestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = TestController.class)
+@Import(GlobalErrorHandler.class)
+class GlobalErrorHandlerTest {
+    
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void IllegalArgumentException_처리() throws Exception {
+        mockMvc.perform(get("/illegal-arg"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("잘못된 요청입니다: 파라미터 오류"));
+    }
+
+    @Test
+    void EmailSendException_처리() throws Exception {
+        mockMvc.perform(get("/email-error"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().string("이메일 전송 실패: SMTP 서버 오류"));
+    }
+}
