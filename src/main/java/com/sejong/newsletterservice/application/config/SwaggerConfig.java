105ab86c6269b489894e025dcb2670f2f761ca96
PlusUltraCode <kkd06144@naver.com>
2025-06-30 14:41:57 +0900

    #11 feat: swagger 등록 추가

--- src/main/java/com/sejong/newsletterservice/application/config/SwaggerConfig.java
+++ src/main/java/com/sejong/newsletterservice/application/config/SwaggerConfig.java
@@ -2,26 +2,25 @@ package com.sejong.newsletterservice.application.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title = "Sample API",
-                description = "Swagger 테스트.",
-                version = "v1"))
-@RequiredArgsConstructor
+        info = @Info(
+                title = "Newsletter API",
+                version = "v1",
+                description = "뉴스레터 API 문서입니다."
+        )
+)
 @Configuration
-public class SwaggerConfig{
+public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi SampleUserOpenApi() {
-        String[] paths = {"/**"};
-
-        return GroupedOpenApi.builder()
-                .group("com.example")
-                .pathsToMatch(paths)
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI().addServersItem(new Server().url("/"));
     }
 }
--- src/main/java/com/sejong/newsletterservice/application/exception/GlobalErrorHandler.java
+++ src/main/java/com/sejong/newsletterservice/application/exception/GlobalErrorHandler.java
@@ -1,11 +1,19 @@
 package com.sejong.newsletterservice.application.exception;
 
+import com.sejong.newsletterservice.application.email.NewsletterScheduler;
+import com.sejong.newsletterservice.application.subscriber.SubscriberController;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@Slf4j
+@Hidden
+@RestControllerAdvice
 public class GlobalErrorHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
