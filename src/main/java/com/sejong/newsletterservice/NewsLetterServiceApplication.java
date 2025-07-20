7db923294747a27bfe0c5664e4b99d6ce1585ea6
PlusUltraCode <kkd06144@naver.com>
2025-07-20 17:23:56 +0900

    chore : cicd 테스트 3

--- src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
+++ src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
@@ -2,7 +2,6 @@ package com.sejong.newsletterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
