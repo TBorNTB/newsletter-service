1162c5254a3a08d62f5c7176e3a9b535704cb6f1
PlusUltraCode <kkd06144@naver.com>
2025-08-07 17:18:54 +0900

    #16 chore : 애플리케이션 진입점 설정 수정

--- src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
+++ src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
@@ -2,12 +2,14 @@ package com.sejong.newsletterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
+@EnableFeignClients
 public class NewsLetterServiceApplication {
 
     public static void main(String[] args) {
