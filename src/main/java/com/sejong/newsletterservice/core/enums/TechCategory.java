e2c76770351d480cd911fd99bc03338ea77068d8
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-09-14 15:15:02 +0900

    feat: 구독할 카테고리 추가

---
+++ src/main/java/com/sejong/newsletterservice/core/enums/TechCategory.java
@@ -0,0 +1,11 @@
+package com.sejong.newsletterservice.core.enums;
+
+public enum TechCategory {
+    SYSTEM_HACKING,
+    WEB_HACKING,
+    DIGITAL_FORENSICS,
+    REVERSING,
+    CRYPTOGRAPHY,
+    NETWORK_SECURITY,
+    IOT_SECURITY
+}
