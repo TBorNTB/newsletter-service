d3a1b9c7b329329568fd9d0bd84141608b623c1b
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-20 15:59:45 +0900

    feat: 더미데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,24 @@
 --  subscriber
-INSERT IGNORE INTO subscriber (id, created_at, email, email_frequency)
+INSERT IGNORE INTO subscriber (created_at, email, email_frequency)
 VALUES
-  (1, '2025-06-21 07:33:02.597425', 'kkd06144@naver.com', 'DAILY'),
-  (4, '2025-06-22 16:15:20.285494', 'kkd06155@naver.com', 'DAILY'),
-  (5, '2025-06-22 19:53:13.639288', 'drsoombre@naver.com', 'DAILY'),
-  (6, '2025-06-22 19:54:13.257348', 'sisubada@sju.ac.kr', 'DAILY'),
-  (7, '2025-06-22 19:54:55.982282', 'haeun4345@naver.com', 'DAILY');
+  ('2025-01-15 09:00:00', 'drsoombre@naver.com', 'DAILY'),
+  ('2025-01-16 10:30:00', 'kkd06144@naver.com', 'WEEKLY'),
+  ('2025-01-17 14:20:00', 'user3@sju.ac.kr', 'DAILY'),
+  ('2025-01-18 16:45:00', 'user4@naver.com', 'WEEKLY'),
+  ('2025-01-19 11:10:00', 'user5@gmail.com', 'DAILY'),
+  ('2025-01-20 13:25:00', 'user6@sju.ac.kr', 'WEEKLY');
 
 --  mailcategory
 INSERT IGNORE INTO mailcategory (subscriber_id, category_name)
 VALUES
-  (1, 'CRYPTOGRAPHY'),
-  (4, 'SYSTEM_HACKING'),
+  (1, 'SYSTEM_HACKING'),
+  (1, 'WEB_HACKING'),
+  (2, 'CRYPTOGRAPHY'),
+  (2, 'NETWORK_SECURITY'),
+  (3, 'DIGITAL_FORENSICS'),
+  (4, 'REVERSING'),
+  (4, 'IOT_SECURITY'),
+  (5, 'WEB_HACKING'),
   (5, 'CRYPTOGRAPHY'),
-  (6, 'CRYPTOGRAPHY'),
-  (7, 'CRYPTOGRAPHY');
+  (5, 'NETWORK_SECURITY'),
+  (6, 'SYSTEM_HACKING');
