2fbaaffd79411cee726c8b49570b66f2a5d649b4
PlusUltraCode <kkd06144@naver.com>
2026-03-01 16:51:07 +0900

    세계시 -> 한국시로 변경

--- src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
+++ src/main/java/com/sejong/newsletterservice/NewsLetterServiceApplication.java
@@ -1,16 +1,24 @@
 package com.sejong.newsletterservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
 @EnableFeignClients
 public class NewsLetterServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NewsLetterServiceApplication.class, args);
